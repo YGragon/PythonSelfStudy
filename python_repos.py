0d1c2d172fc145a09974a5c4bc8c1bab194bb18c
dongxi346 <1105894953@qq.com>
2017-12-11 22:43:28 +0800

    将Github上Python点赞最多的库可视化

--- python_repos.py
+++ python_repos.py
@@ -1,7 +1,9 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用，并存储响应
-url = 'https://api.github.com/search/repositories?q=tetris+language:python&sort=stars&order=desc'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:", r.status_code)
 
@@ -13,21 +15,37 @@ print("Total repositores:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositores returned:", len(repo_dicts))
 
+names, stars = [], []
+
+
 # 研究第一个仓库
 repo_dict = repo_dicts[0]
 # print("\nSelected information about first repository:")
 
 print("\nSelected information about each repository:")
 for repo_dict in repo_dicts:
-    print('Name-项目名:', repo_dict['name'])
-    print('Owner-作者:', repo_dict['owner']['login'])
-    print('Stars-点赞数:', repo_dict['stargazers_count'])
-    print('Repositores-项目地址:', repo_dict['html_url'])
-    print('Create-创造时间:', repo_dict['created_at'])
-    print('Updated-更新时间:', repo_dict['updated_at'])
-    print('Description-描述:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-print("\nKeys:", len(repo_dict))
+# 可视化
+
+my_style = LS('#333366', base_style = LCS)
+chart = pygal.Bar(style = my_style, x_label_rotation = 45, show_legend = False)
+chart.title = "GitHub 上点赞最多的仓库"
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+
+#     print('Name-项目名:', repo_dict['name'])
+#     print('Owner-作者:', repo_dict['owner']['login'])
+#     print('Stars-点赞数:', repo_dict['stargazers_count'])
+#     print('Repositores-项目地址:', repo_dict['html_url'])
+#     print('Create-创造时间:', repo_dict['created_at'])
+#     print('Updated-更新时间:', repo_dict['updated_at'])
+#     print('Description-描述:', repo_dict['description'])
+
+# print("\nKeys:", len(repo_dict))
 # for key in sorted(repo_dict.keys()):
 #     print(key)
 
