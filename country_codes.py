9c983ead97d888beec8515220de4f1ba5df5d0c9
dongxi346 <1105894953@qq.com>
2017-12-13 22:53:41 +0800

    分析2010年世界人口

--- country_codes.py
+++ country_codes.py
@@ -2,8 +2,11 @@ from pygal.i18n import COUNTRIES
 
 def get_country_code(country_name):
     """根据指定的国家，返回Pygal使用的两个字母的国别码"""
+    error_countys = []
     for code, name in COUNTRIES.items():
         if name == country_name:
             return code
+
+
     # 如果没有找到指定的国家，则返回None
     return None
