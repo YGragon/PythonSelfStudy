07cf1e663259e93fbde1d5baed65a87290904c8e
dongxi346 <1105894953@qq.com>
2018-01-17 23:23:00 +0800

    添加错误处理

--- data_collection/scrapetest.py
+++ data_collection/scrapetest.py
@@ -1,17 +1,23 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError, URLError
 
-try:
-    # url 可能返回为空，或者 url 输入错误
-    html = urlopen("http://pythonscraping.com/pages/page1.html")
-except Exception as e:
-    print(e)
-else:
-    if html is None:
-        print("URL is not found")
-    else:
+def getTitle(url):
+    try:
+        # url 可能返回为空，或者 url 输入错误
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        return None
+    try:
         # BeautifulSoup 需要添加第二参数：'lxml'，不然会有警告, 参数二是个库，需要安装
         bsObj = BeautifulSoup(html.read(),'lxml')
-        # print(html.read())
-        print(bsObj.h1)
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
 
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
