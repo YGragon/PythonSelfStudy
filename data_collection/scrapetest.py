5bed977d3131b11acee87580ceaaf72a92c51232
dongxi346 <1105894953@qq.com>
2018-01-17 23:32:41 +0800

    添加爬取标题和人名

--- data_collection/scrapetest.py
+++ data_collection/scrapetest.py
@@ -2,7 +2,21 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError, URLError
 
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+
+nameList = get_person_name("http://www.pythonscraping.com/pages/warandpeace.html")
+if nameList == None:
+    print("nameList could not be found")
+else:
+    for name in nameList:
+        print(name.get_text())
+
 def getTitle(url):
+    """获取标题"""
     try:
         # url 可能返回为空，或者 url 输入错误
         html = urlopen(url)
@@ -16,8 +30,18 @@ def getTitle(url):
         return None
     return title
 
-title = getTitle("http://pythonscraping.com/pages/page1.html")
-if title == None:
-    print("Title could not be found")
-else:
-    print(title)
+def get_person_name(url):
+    """获取绿色的人名"""
+    try:
+        # url 可能返回为空，或者 url 输入错误
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        return None
+    try:
+        # BeautifulSoup 需要添加第二参数：'lxml'，不然会有警告, 参数二是个库，需要安装
+        bsObj = BeautifulSoup(html.read(),'lxml')
+        nameList = bsObj.findAll("span",{"class":"green"})
+    except AttributeError as e:
+        return None
+    return nameList
+
