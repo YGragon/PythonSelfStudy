2e62adbbbf8c7ca0634eeb28cc8179638882993d
dongxi346 <1105894953@qq.com>
2018-01-21 22:55:27 +0800

    添加获取链接

--- data_collection/collecttext.py
+++ data_collection/collecttext.py
@@ -4,3 +4,15 @@ import datetime
 import random
 import re
 
+random.seed(datetime.datetime.now())
+
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
+    bsObj = BeautifulSoup(html)
+    return bsObj.find("div",{"id":"bodyContent"}).findAll("a",href=re.compile(("^(/wiki/)((?!:).)*$")))
+
+links = getLinks("/wiki/Kevin_Bacon")
+while  len(links) > 0:
+    newArticle = links[random.randint(0,len(lenks)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
