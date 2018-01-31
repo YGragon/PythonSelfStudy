72536ec3ac7b316d976d6b355fd60e6788484889
dongxi346 <1105894953@qq.com>
2018-01-31 23:29:50 +0800

    添加 json api 的使用

--- data_collection/apiLearning.py
+++ data_collection/apiLearning.py
@@ -6,4 +6,19 @@ def getCountry(ipAddress):
     responseJson = json.loads(response)
     return responseJson.get("country_code")
 
+def getGankInfo():
+    filename = 'gank_android.json'
+    with open(filename) as f:
+        gank_info = json.load(f)
+
+    for gank_data in gank_info:
+        who = gank_data['who']
+        used = gank_data['used']
+        print("who: "+who+"----used: "+used)
+
+
+getGankInfo()
+
 print(getCountry("50.78.253.58"))
+
+
